589b27007f8f2871f9bcd89c86a8fa74fe6a0ad7
최광진 <38902367+arattha@users.noreply.github.com>
2023-04-19 21:40:28 +0900

    Initial commit

---
+++ src/main/java/com/example/batchapiservice/domain/ApiInfo.java
@@ -0,0 +1,15 @@
+package com.example.batchapiservice.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ApiInfo {
+
+    private String url;
+    private List<? extends ApiRequestVO> apiRequestList;
+
+}
